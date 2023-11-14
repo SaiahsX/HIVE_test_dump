ade4dce4820da41fb69ec327abb6e8a4a063b713
Oluwaseun Sunday Adewumi <Saiahs.ent@gmail.com>
2023-11-14 19:36:18 +0200

    Add files via upload

---
+++ ft_atoi.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oadewumi <oadewumi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/14 14:14:27 by oadewumi          #+#    #+#             */
+/*   Updated: 2023/11/14 19:33:51 by oadewumi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	size_t	output;
+	int		indx;
+	int		polarity;
+
+	output = 0;
+	indx = 0;
+	polarity = 1;
+	while (str[indx] == 32 || (str[indx] >= 9 && str[indx] <= 13))
+		indx++;
+	if (str[indx] == '-' || str[indx] == '+')
+	{
+		if (str[indx] == '-')
+			polarity *= -1;
+		indx++;
+	}
+	while (ft_isdigit(str[indx]))
+	{
+		output = output * 10 + (str[indx] - '0');
+		if (output > SIZE_MAX / 2 && polarity < 0)
+			return (0);
+		else if (output > SIZE_MAX / 2 && polarity > 0)
+			return (-1);
+		indx++;
+	}
+	return ((int) output * polarity);
+}
+
+#include <stdio.h>
+
+int	main(int ac, char **av)
+{
+	int	mine;
+	int	org;
+	if (ac == 2)
+	{
+		mine = ft_atoi(av[1]);
+		org = atoi(av[1]);
+		printf("mine: %d | org: %d\n", mine, org);
+	}
+	return (0);
+}
